8d4ce600508e75779aee352b7b22d95dc6863c94
pwyq <y433wu@edu.uwaterloo.ca>
2019-10-17 14:16:50 +0200

    init

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# File              : run.sh
+# Author            : Yanqing Wu <meet.yanqing.wu@gmail.com>
+# Date              : 17.10.2019
+# Last Modified Date: 17.10.2019
+# Last Modified By  : Yanqing Wu <meet.yanqing.wu@gmail.com>
+
+#rm -rf build
+#mkdir build
+cd build
+#cmake ..
+make -j8
+cd ..
+#rm *.ppm
+MESA_GL_VERSION_OVERRIDE=4.3 ./build/raytracing
